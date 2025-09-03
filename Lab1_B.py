62219c252cb41559b9e65e67d6740cb5a2183891
Sandwich-Monsieur <dommiafl@gmail.com>
2025-09-03 09:55:43 -0400

    Delete Lab1_B.py

--- Lab1_B.py
+++
@@ -1,6 +0,0 @@
-itemPrice = float(input("Enter the price of the item: "))
-salesTax = float(input("Enter the sales tax percentage: "))
-
-total = itemPrice * (1 + salesTax / 100)
-
-print("Your total is $", f"{total:.2f}", sep="")
