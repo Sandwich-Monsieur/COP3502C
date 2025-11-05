f966e303adaca0cd6596e4b4d8683c6941ef6a8c
Sandwich-Monsieur <dommiafl@gmail.com>
2025-11-05 08:47:34 -0500

    Finished Project 2 Part B and uploading Review work

---
+++ PyCharm/Module 10/CowLab II/cowsay.py
@@ -0,0 +1,47 @@
+import sys
+from heifer_generator import get_cows
+from dragon import Dragon
+
+def list_cows(cows):
+    names = [cow.get_name() for cow in cows]
+    return names
+
+def find_cow(name, cows):
+    for cow in cows:
+        if cow.get_name() == name:
+            return cow
+    return None
+
+
+def main():
+
+    if sys.argv[1] == "-l":
+        names = list_cows(get_cows())
+        print("Cows available:", " ".join(names))
+
+    elif sys.argv[1] == "-n":
+        cow = find_cow(sys.argv[2], get_cows())
+        message = sys.argv[3:]
+        if cow is None:
+            print("Could not find", str(sys.argv[2]), "cow!")
+        else:
+            print(" ".join(message))
+            print(cow.get_image())
+
+            if isinstance(cow, Dragon):
+                if cow.can_breath_fire():
+                    print("This dragon can breathe fire.")
+                else:
+                    print("This dragon cannot breathe fire.")
+
+    else:
+        message = sys.argv[1:]
+        default_cow = find_cow("heifer", get_cows())
+        print(" ".join(message))
+        print(default_cow.get_image())
+
+
+
+
+if __name__ == "__main__":
+    main()
